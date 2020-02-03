a7ddd56d13bc2eb506fdcafe8d75283f79ced6be
Josh Cosson <joshlcosson@gmail.com>
2020-02-02 16:44:22 -0800

    Saving Heroku Changes

---
+++ client/build/precache-manifest.6f2a2a1b269fc75eeaf424137ba7317d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d672f040abfa3c0ea28a",
+    "url": "/google-books-search/static/js/runtime~main.9ad78463.js"
+  },
+  {
+    "revision": "ac8e4bd5b40a900b0b86",
+    "url": "/google-books-search/static/js/main.5eddc4de.chunk.js"
+  },
+  {
+    "revision": "104b0bba0549592f3765",
+    "url": "/google-books-search/static/js/2.8983c58d.chunk.js"
+  },
+  {
+    "revision": "92bbac4a3b217535464fb975215d88f7",
+    "url": "/google-books-search/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -3,20 +3,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Add routes, both API and view
+app.use("/", routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks");
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "client", "build", "index.html")); //relative path
+  });
+}
 
 // Start the API server
 app.listen(PORT, function() {
